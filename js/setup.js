3998c2467903a3aa2f485911042eee50dee5916d
LexiosAlex <lexiosma@gmail.com>
2018-11-23 04:58:10 +0300

    Изменил имена переменных

--- js/setup.js
+++ js/setup.js
@@ -9,56 +9,55 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var wizzardFirstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var wizzardSurname = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var wizzardFirstNameArr = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizzardSurnameArr = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var getWizzardName = function(nameArr, surnameArr) {
-  function compareRandom(a, b) {
-    return Math.random() - 0.5;
-  };
+function compareRandom() {
+  return Math.random() - 0.5;
+}
 
+var getWizzardName = function (nameArr, surnameArr) {
   nameArr.sort(compareRandom);
   surnameArr.sort(compareRandom);
-  var Name = [];
-  for (var i = 0; i < nameArr.length; i++){
-    Name[i] = nameArr[i] + ' ' +  surnameArr[i];
+  var name = [];
+  for (var i = 0; i < nameArr.length; i++) {
+    name[i] = nameArr[i] + ' ' + surnameArr[i];
   }
-  return Name;
-}
+  return name;
+};
 
-var wizzardName = getWizzardName(wizzardFirstName, wizzardSurname);
+var wizzardName = getWizzardName(wizzardFirstNameArr, wizzardSurnameArr);
 
-var wizzardCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var wizzardEyeColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizzardCoatColorArr = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var wizzardEyeColorArr = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomArrValue = function(arr) {
-  function compareRandom(a, b) {
-    return Math.random() - 0.5;
-  };
-
+var getRandomArrValue = function (arr) {
   return arr.sort(compareRandom);
-}
+};
+
+var wizzardCoatColor = getRandomArrValue(wizzardCoatColorArr);
+var wizzardEyeColor = getRandomArrValue(wizzardEyeColorArr);
 
 var wizards = [
   {
     name: wizzardName[0],
-    coatColor: getRandomArrValue(wizzardCoatColor)[0],
-    eyesColor: getRandomArrValue(wizzardEyeColor)[0]
+    coatColor: wizzardCoatColor[0],
+    eyesColor: wizzardEyeColor[0]
   },
   {
     name: wizzardName[1],
-    coatColor: getRandomArrValue(wizzardCoatColor)[1],
-    eyesColor: getRandomArrValue(wizzardEyeColor)[1]
+    coatColor: wizzardCoatColor[1],
+    eyesColor: wizzardEyeColor[1]
   },
   {
     name: wizzardName[2],
-    coatColor: getRandomArrValue(wizzardCoatColor)[2],
-    eyesColor: getRandomArrValue(wizzardEyeColor)[2]
+    coatColor: wizzardCoatColor[2],
+    eyesColor: wizzardEyeColor[2]
   },
   {
     name: wizzardName[3],
-    coatColor: getRandomArrValue(wizzardCoatColor)[3],
-    eyesColor: getRandomArrValue(wizzardEyeColor)[3]
+    coatColor: wizzardCoatColor[3],
+    eyesColor: wizzardEyeColor[3]
   }
 ];
 
@@ -71,7 +70,7 @@ var renderWizard = function (wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
